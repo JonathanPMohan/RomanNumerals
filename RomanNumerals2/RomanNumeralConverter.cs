d6f44193012568fa0132e32bb051bcfdb04cb3e7
unknown <jonathanmohan.net@gmail.com>
2019-04-06 12:20:38 -0500

    adding comments

--- RomanNumerals2/RomanNumeralConverter.cs
+++ RomanNumerals2/RomanNumeralConverter.cs
@@ -7,39 +7,46 @@ namespace RomanNumerals2
 {
     public class RomanNumeralConvertor
     {
-        public string ConvertToRoman(int convertThis)
+        // Building Roman Numeral String To Be Converted From Number Input //
+        public string ConvertToRoman(int convertNumber)
         {
-            int leftovers;              //store mod results
-            string RomanNumeral = "";   //store roman numeral string
-            Dictionary<string, int> dict = new Dictionary<string, int>()
+            int leftovers;              //store mod results //
+            string RomanNumeral = "";   //store roman numeral as a string //
+            
+            // Build A Dictionary To House The Roman Numeral as a STRING and the Number as a Interger //
+            Dictionary<string, int> NumeralConverter = new Dictionary<string, int>()
         {
-            {"M", 1000},// 1000 = M
-            {"CM", 900},// 900 = CM
-            {"D", 500}, // 500 = D
-            {"CD", 400},// 400 = CD
-            {"C", 100}, // 100 = C
-            {"XC", 90}, // 90 = XC
-            {"L", 50},  // 50 = L
-            {"XL", 40}, // 40 = XL
-            {"X", 10},  // 10 = X
-            {"IX", 9},  // 9 = IX
-            {"V", 5},   // 5 = V
-            {"IV", 4},  // 4 = IV
-            {"I", 1},   // 1 = I
+            {"M", 1000},    // 1000 = M
+            {"CM", 900},    // 900 = CM
+            {"D", 500},     // 500 = D
+            {"CD", 400},    // 400 = CD
+            {"C", 100},     // 100 = C
+            {"XC", 90},     // 90 = XC
+            {"L", 50},      // 50 = L
+            {"XL", 40},     // 40 = XL
+            {"X", 10},      // 10 = X
+            {"IX", 9},      // 9 = IX
+            {"V", 5},       // 5 = V
+            {"IV", 4},      // 4 = IV
+            {"I", 1},       // 1 = I
         };
-            foreach (KeyValuePair<string, int> pair in dict)
+            // For Each Loop to Loop Over Each Key Value Pair In Dictionary //
+            foreach (KeyValuePair<string, int> NumeralNumberPair in NumeralConverter)
             {
-                if (convertThis >= pair.Value)
+                // if number is greater than or equal to Numeral/Numbers Pair in Dictionary // 
+                if (convertNumber >= NumeralNumberPair.Value)
                 {
-                    leftovers = convertThis % pair.Value;
-                    int remainder = (convertThis - leftovers) / pair.Value;
-                    convertThis = leftovers;
+                    leftovers = convertNumber % NumeralNumberPair.Value;
+                    int remainder = (convertNumber - leftovers) / NumeralNumberPair.Value;
+                    convertNumber = leftovers;
                     while (remainder > 0)
                     {
-                        RomanNumeral += pair.Key; remainder--;
+                        RomanNumeral += NumeralNumberPair.Key; remainder--;
                     }
                 }
             }
+
+            // Return the Converted Roman Numeral String //
             return RomanNumeral;
         }
     }
